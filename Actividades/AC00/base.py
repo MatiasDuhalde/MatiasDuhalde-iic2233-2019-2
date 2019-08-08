f0801566030f2f349038ce154059fd9e8223dbb1
Matías Duhalde <46191755+MatiasDuhalde@users.noreply.github.com>
2019-08-08 16:27:46 -0400

    Add base.py
    Se agrega la base sobre la cual trabajar, falta implementar los métodos.

---
+++ Actividades/AC00/base.py
@@ -0,0 +1,36 @@
+import math
+
+class Triangulo:
+	def __init__(self, p1, p2, p3):
+		self.p1 = p1
+		self.p2 = p2
+		self.p3 = p3
+		
+	def obtener_area(self):
+		pass
+		
+	def obtener_perimetro(self):
+		pass
+		
+	def es_equilatero(self):
+		pass
+		
+	def __str__(self):
+		pass
+		
+		
+class Cuadrado:
+	def __init__(self, p1, p2, p3, p4):
+		self.p1 = p1
+		self.p2 = p2
+		self.p3 = p3
+		self.p4 = p4
+	
+	def obtener_area(self):
+		pass
+		
+	def obtener_perimetro(self):
+		pass
+		
+	def __str__(self):
+		pass
