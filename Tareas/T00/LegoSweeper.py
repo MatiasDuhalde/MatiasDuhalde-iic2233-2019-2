8a1ef62030b97a186b6ae3b5845951cd4813fe48
Matias Duhalde <matias.duhalde@uc.cl>
2019-08-10 05:20:10 -0400

    Add LegoSweeper.py
    This file will contain the menus and the game in future commits.

---
+++ Tareas/T00/LegoSweeper.py
@@ -0,0 +1,15 @@
+from parametros import PROB_LEGO, POND_PUNT
+from tablero import print_tablero
+import os
+import math
+import random
+
+
+def menu_inicio():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    pass
