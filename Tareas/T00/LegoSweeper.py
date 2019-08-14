2cc5c31ab93c0ce2cc7c7008d6a0d48dd12bafd9
Matias Duhalde <matias.duhalde@uc.cl>
2019-08-14 01:34:22 -0400

    Update LegoSweeper.py
    Added in-game input handling, tile recognition/analysis, in-game exit
    options (save, don't save, back), an some other small changes.
    Load/Save game still in early stages.

--- Tareas/T00/LegoSweeper.py
+++ Tareas/T00/LegoSweeper.py
@@ -6,20 +6,82 @@ import math
 import gametext
 import sys
 
+# NEXT FEATURES:
+# Add highscore to in game screen
+# Prevent user from losing in first reveal
+
+# =============================================================================
+#                          BOARD HANDLING FUNCTIONS
+# =============================================================================
+
+let_to_num = {} 
+
+
+class Tablero:
+
+    def __init__(self, largo, ancho, legos = []):
+        self.largo = largo
+        self.ancho = ancho
+        self.legos = legos
+
+        self.tablero = []
+        for _ in range(largo):
+            self.tablero.append([" "] * ancho)
+        
+        if self.legos == []:
+            n_legos = math.ceil(largo * ancho * PROB_LEGO)
+            self.legos = random.sample(range(largo * ancho), n_legos)
+
+    def pos_to_num(self, fil, col):
+        return self.ancho * fil + col
+
+    def num_to_pos(self, num):
+        # (fil, col)
+        return (num // self.ancho, num % self.ancho)
+    
+    def lego_in_tile(self, fil, col):
+        if self.pos_to_num(fil, col) in self.legos:
+            return True
+        return False
+
+    def check_tile(self, fil, col):
+        if self.lego_in_tile(fil, col):
+            self.reveal_legos()
+            return "L"
+        else: 
+            # COMPLETAR
+            pass
+    
+    def reveal_legos(self):
+        for i in self.legos:
+            coords = self.num_to_pos(i)
+            self.tablero[coords[0]][coords[1]] = "L"
+
+    # COMPLETAR
+    # COMPLETAR
+    # COMPLETAR
+    def guardar(self, username):
+        pass
+
+    def show(self):
+        print_tablero(self.tablero)
+
+
+
+
 # =============================================================================
 #                              MISC. FUNCTIONS
 # =============================================================================
 
-
-# LIMPIA LA PANTALLA, PARA HACER MEJOR LAS TRANSICIONES
+# Limpia la pantalla, para hacer mejores las transiciones
 # Snippet basado en respuesta de @popcnt en:
 # https://stackoverflow.com/questions/517970/how-to-clear-the-interpreter-console
 def clear_screen():
     _ = os.system("cls" if os.name == "nt" else "clear")
     return None
 
-# RECIBE UN INPUT ESTRICTAMENTE NUMÉRICO ENTRE DOS VALORES DADOS. ESPECIALMENTE
-# ÚTIL EN LOS MENÚS
+# Recibe un input estrictamente numérico (entero) entre 2 otros enteros.
+# Opcionalmente recibe también el cero. Rechaza no-enteros o fuera de rango.
 def get_int_input(a, b, prompt="Ingrese un número: ", accept_zero=False):
     n = input(prompt)
     if n.isdigit():
@@ -37,7 +99,7 @@ def get_int_input(a, b, prompt="Ingrese un número: ", accept_zero=False):
         return None
     return None
 
-# ORGANIZA LA SCOREBOARD DE MANERA DESCENDIENTE
+# Organiza la scoreboard de manera descendiente
 def sort_scoreboard():
     temp = []
     f = open("puntajes.txt", "r")
@@ -50,24 +112,6 @@ def sort_scoreboard():
         f.write(",".join(line))
     f.close
     return None
-
-
-# =============================================================================
-#                          BOARD HANDLING FUNCTIONS
-# =============================================================================
-
-
-# En el enunciado está implícito que se debe ocupar math.ceil para aproximar
-# la cantidad de legos
-def crear_tablero(largo, ancho):
-    n_legos = math.ceil(largo * ancho * PROB_LEGO)
-    pos_legos = random.sample(range(largo * ancho), n_legos)
-    print(pos_legos)
-    tablero = []
-    for _ in range(largo):
-        tablero.append([" "] * ancho)
-    return tablero, pos_legos
-
 
 # =============================================================================
 #                             MENU FUNCTIONS
@@ -88,6 +132,7 @@ def menu_inicio():
             clear_screen()
             return n
 
+
 def menu_nueva_partida():
     username = ""
     largo = ""
@@ -96,7 +141,7 @@ def menu_nueva_partida():
     while status != 4:
         print(gametext.BRICKS)
         print("{:>15}: {}\n{:>15}: {}\n{:>15}: {}\n{:>40}".format("JUGADOR", 
-        username, "Largo", largo, "Ancho", ancho, "[0]  Salir"))
+        username, "Largo", largo, "Ancho", ancho, "[0]  Volver"))
         print(gametext.BRICKS)
 
         if status == 0:
@@ -135,18 +180,85 @@ def menu_nueva_partida():
 
     if status == 4:
         input("Presione ENTER para comenzar la partida...")
-        clear_screen()
         return (username, largo, ancho)
     clear_screen()
     return None
 
+
+#PENDIENTE
+#PENDIENTE
+#PENDIENTE
+#PENDIENTE
 def menu_cargar_partida():
+    print(gametext.BRICKS)
+    print(gametext.LEGOBRICK)
+    print("{:>15}    {}".format("[0]", "Volver"))
+    print(gametext.BRICKS)
+    username = input("\nInserte su nombre: ")
+    if username != "0":
+        pass
+    clear_screen()
+
+#PENDIENTE
+#PENDIENTE
+#PENDIENTE
+#PENDIENTE
+def main_game(username, largo, ancho):
+    t = Tablero(largo, ancho)
+    score = 0
+    game_cycle = True
+    while game_cycle:
+        clear_screen()
+        print(gametext.BRICKS)
+        print("{:^35}{:^9}{:^35}".format("JUGADOR", gametext.MIDSEP1, "PUNTOS"))
+        print("{:^35}{:^9}{:^35}".format(username, gametext.MIDSEP2, score))
+        print(gametext.BRICKS)
+        print(t.legos)
+        t.show()
+        print(gametext.BRICKS)
+        print(gametext.GAMEMENU_OPTIONS)
+        print(gametext.BRICKS)
+        user_choice = input("Ingrese un comando: ")
+
+        if user_choice == "0":
+            exit_cycle = True
+            clear_screen()
+            while exit_cycle:
+                print(gametext.BRICKS)
+                print(gametext.EXITPROMPT_OPTIONS)
+                print(gametext.BRICKS)
+                exit_choice = get_int_input(0, 2)
+                if exit_choice == 1:
+                    clear_screen()
+                    t.guardar(username)
+                    exit_cycle = False
+                    game_cycle = False
+                elif exit_choice == 2:
+                    exit_cycle = False
+                    game_cycle = False
+                elif exit_choice == 0:
+                    exit_cycle = False
+            clear_screen()
+        
+        elif user_choice == "1":
+            t.guardar(username)
+        
+        elif "-" in user_choice:
+            coords = user_choice.split("-")
+            if len(coords) == 2 and coords[1].isdigit() and \
+                coords[0].isalpha() and len(coords[0]) == 1:
+                col = ord(coords[0].lower()) - 97
+                fil = int(coords[1])
+                tile = t.check_tile(fil, col)
+                if tile == "L":
+                    game_cycle = False
+    return username, score, t
+                
+def end_screen(username, score, tablero):
     pass
 
-def main_game(username, largo, ancho):
-    tablero, pos_legos = crear_tablero(largo, ancho)
-    print_tablero(tablero)
 
+# Muestra primeros 10 puntajes guardados en puntajes.txt
 def scoreboard():
     clear_screen()
     print(gametext.BRICKS)
@@ -202,10 +314,10 @@ if __name__ == '__main__':
         if user_choice == 1:
             datos_juego = menu_nueva_partida()
             if datos_juego != None:
-                main_game(*datos_juego)
-    
+                username, score, tablero = main_game(*datos_juego)
+                end_screen(username, score, tablero)
         elif user_choice == 2:
-            pass
+            menu_cargar_partida()
 
         elif user_choice == 3:
             scoreboard()
