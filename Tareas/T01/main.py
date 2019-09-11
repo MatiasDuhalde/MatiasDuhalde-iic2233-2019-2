d273841e6e1e5ca52b079dac03bacb3a7488e73b
Matias Duhalde <matias.duhalde@uc.cl>
2019-09-11 03:20:15 -0300

    Modify main implementation and game flow

--- Tareas/T01/main.py
+++ Tareas/T01/main.py
@@ -1,8 +1,10 @@
 from lib.funciones import clear
-from lib.menu import MenuSesion, MenuPrincipal, MenuCarrera, MenuCompraVehiculos, \
-    MenuPits, MenuPreparacionCarrera
+from lib.menu import MenuSesion, MenuPrincipal, MenuCarrera, \
+    MenuCompraVehiculos, MenuPits, MenuPreparacionCarrera
 
-
+# -----------------------------------------------------------------------------
+#                                 GAME FLOW
+# -----------------------------------------------------------------------------
 menu_sesion = MenuSesion()
 print(menu_sesion)
 a = menu_sesion.recibir_input()
