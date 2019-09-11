79ceb140961ef01fbfd442b1f1c705f129e8b4ee
Matias Duhalde <matias.duhalde@uc.cl>
2019-09-11 03:20:53 -0300

    Add cargar_partida method to MenuSesion

--- Tareas/T01/lib/menu.py
+++ Tareas/T01/lib/menu.py
@@ -1,12 +1,19 @@
+import os
 from abc import ABC, abstractmethod
+
 import lib.gametext as gametext
 from lib.funciones import clear
+from lib.carreras import Tareo, Docencio, Hibrido
 
 
 
 class Menu(ABC):
     """Abstract class. Inherited by more specific menu classes."""
     
+    teams_pilotos = {"Tareos" : Tareo, 
+    "Hibrido" : Hibrido, 
+    "Docencios" : Docencio}
+
     @abstractmethod
     def __init__(self):
         self.actions = {}
@@ -42,6 +49,7 @@ class Menu(ABC):
 
     @abstractmethod
     def __str__(self):
+        """Meant to be used with print(). Shows the menu"""
         string = str(self.get_str())
         return string
 
@@ -56,9 +64,29 @@ class MenuSesion(Menu):
         })
 
     def cargar_partida(self):
-        pass
+        """Return Piloto object based on user input."""
+        while True:
+            clear()
+            print(gametext.LOAD_TITLE)
+            print(gametext.SEP)
+            
+            name = input("Introduzca su nombre: ")
+            
+            if name == "0":
+                break
+            with open(os.path.join('..', 'databases', 'pilotos.csv'), 'r') \
+                as pilotos:
+                pilotos.readline()
+                for line in pilotos:
+                    if name in line:
+                        line = line.rstrip().split(",")
+                        piloto = teams_pilotos[line[-1]](*line)
+                        if name == piloto.nombre:
+                            return piloto
 
-    def crear_partida(self):
+
+
+    def nueva_partida(self):
         pass
 
     def __str__(self):
