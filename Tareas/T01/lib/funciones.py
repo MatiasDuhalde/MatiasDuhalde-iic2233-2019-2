71d5aaa406693c36d9e4aea26c7fd9172e8accd2
Matias Duhalde <matias.duhalde@uc.cl>
2019-09-11 16:14:38 -0300

    Add savegame loading auxiliary functions

--- Tareas/T01/lib/funciones.py
+++ Tareas/T01/lib/funciones.py
@@ -6,3 +6,50 @@ def clear():
         os.system('cls')
     else:
         os.system('clear')
+
+
+def get_vehiculos(name):
+    """Returns a list containing vehicle objects, that belong to name."""
+    tipos_vehiculo = {"automóvil" : Automovil,
+    "motocicleta" : Motocicleta,
+    "troncomóvil" : Troncomovil,
+    "bicicleta" : Bicicleta}
+
+    vehiculos = []
+    with open(os.path.join('..', 'databases', 'vehículos.csv'), 'r', 
+    encoding='utf-8') as vehiculos:
+        vehiculos.readline()
+        for line in vehiculos:
+            if name in line:
+                line = line.rstrip().split(",")
+                # Line looks like: [Nombre, Dueño, Categoría, Chasis, 
+                # Carrocería, Ruedas, Motor o Zapatillas, Peso]
+                tipo = line.pop(2)
+                vehiculos.append(tipos_vehiculo[tipo](*line))
+    return vehiculos
+
+
+def get_piloto(name):
+    """
+    Returns a pilot object from a name, given it exists in pilotos.csv.
+    Returns None otherwise.
+    """
+    teams_pilotos = {"Tareos" : Tareo, 
+    "Hibrido" : Hibrido, 
+    "Docencios" : Docencio}
+
+    with open(os.path.join('..', 'databases', 'pilotos.csv'), 'r', 
+    encoding='utf-8') as pilotos:
+        pilotos.readline()
+        for line in pilotos:
+            if name in line:
+                line = line.rstrip().split(",")
+                # Line looks like: [Nombre, Dinero, Personalidad, 
+                # Contextura, Equilibrio, Experiencia, Equipo]
+                vehiculos = get_vehiculos(name)
+
+                team = line.pop()
+                piloto = teams_pilotos[team](*line, vehiculos)
+                if name == piloto.nombre:
+                    return piloto
+    return None
