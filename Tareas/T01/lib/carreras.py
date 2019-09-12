7266631bbe7846e2e5485f9838973b60785f1e57
Matias Duhalde <matias.duhalde@uc.cl>
2019-09-12 14:02:22 -0300

    Modify Piloto arguments

--- Tareas/T01/lib/carreras.py
+++ Tareas/T01/lib/carreras.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 from random import randint, choice
 
+# CHECK PARAMETERS IN PARAMETROS.PY
+# THEY MUST BE USED IN HERE (CHECK randints)
+
+
 class Vehiculo(ABC):
     """
     Vehicle modeling class. 
@@ -14,7 +18,7 @@ class Vehiculo(ABC):
     Attributes:
      - Base:
         * chasis
-        * carroceria
+        * carrocería
         * ruedas
      - Specific
         * motor (Automovil, Motocicleta)
@@ -22,41 +26,41 @@ class Vehiculo(ABC):
 
     """
     @abstractmethod
-    def __init__(self, nombre, owner, chasis, carroceria, ruedas, peso):
+    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, peso):
         """Arguments are ordered according to pilotos.csv original data."""
         self.nombre = nombre
-        self.owner = owner
+        self.dueño = dueño
         self.chasis = chasis
-        self.carroceria = carroceria
+        self.carrocería = carrocería
         self.ruedas = ruedas
         self.peso = peso
 
 
 class Automovil(Vehiculo):
     
-    def __init__(self, nombre, owner, chasis, carroceria, ruedas, motor, peso):
-        super().__init__(nombre, owner, chasis, carroceria, ruedas, peso)
+    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, motor, peso):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
         self.motor = motor
 
 
 class Motocicleta(Vehiculo):
     
-    def __init__(self, nombre, owner, chasis, carroceria, ruedas, motor, peso):
-        super().__init__(nombre, owner, chasis, carroceria, ruedas, peso)
+    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, motor, peso):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
         self.motor = motor
 
 
 class Troncomovil(Vehiculo):
     
-    def __init__(self, nombre, owner, chasis, carroceria, ruedas, zapatillas, peso):
-        super().__init__(nombre, owner, chasis, carroceria, ruedas, peso)
+    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, zapatillas, peso):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
         self.zapatillas = zapatillas
 
 
 class Bicicleta(Vehiculo):
     
-    def __init__(self, nombre, owner, chasis, carroceria, ruedas, zapatillas, peso):
-        super().__init__(nombre, owner, chasis, carroceria, ruedas, peso)
+    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, zapatillas, peso):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
         self.zapatillas = zapatillas
 
 
@@ -109,7 +113,7 @@ class Piloto(ABC):
     """
 
     @abstractmethod
-    def __init__(self, nombre, dinero=0, n_exp=0, vehiculos=[]):
+    def __init__(self, nombre, dinero=0, experiencia=0, vehículos=[]):
         """
         Arguments are ordered according to pilotos.csv original data, i.e.:
         
@@ -119,32 +123,32 @@ class Piloto(ABC):
          - Personalidad: personalidad (saved in pilotos.csv, bound to team except Hibrido)
          - Contextura: contextura (saved in pilotos.csv, range bound to team)
          - Equilibrio: equilibrio (saved in pilotos.csv, range bound to team)
-         - Experiencia: nivel_de_experiencia (saved in pilotos.csv) ****
+         - Experiencia: experiencia (saved in pilotos.csv) ****
          - Equipo: (saved in pilotos.csv, implicit in subclass)
          - Vehicle list: vehicles (saved in vehículos.csv) ****
         Note: **** means this should be a property
 
         If a new pilot is created it should only receive a name and a team.
-        New pilots start with dinero = 0, nivel_de_experiencia = 0, vehiculos = []
+        New pilots start with dinero = 0, experiencia = 0, vehículos = []
         """
         self.nombre = nombre
         self.dinero = dinero
-        self.nivel_de_experiencia = n_exp
-        self.vehiculos = vehiculos
+        self.experiencia = experiencia
+        self.vehículos = vehículos
 
 
 class Tareo(Piloto):
     """Tareos team. Inherits from Piloto."""
     
     def __init__(self, nombre, dinero=0, personalidad='precavido', 
-    contextura=randint(26, 45), equilibrio=randint(36, 55), n_exp=0, vehiculos = []):
+    contextura=randint(26, 45), equilibrio=randint(36, 55), experiencia=0, vehículos = []):
         """
         Arguments are ordered according to pilotos.csv original data. i.e.:
         Nombre, Dinero, Personalidad, Contextura, 
         Equilibrio, Experiencia, Equipo,
         Lista de Vehículos (obtained from vehículos.csv)
         """
-        super().__init__(nombre, dinero, n_exp, vehiculos)
+        super().__init__(nombre, dinero, experiencia, vehículos)
         self.contextura = contextura
         self.equilibrio = equilibrio
         self.personalidad = personalidad
@@ -155,14 +159,14 @@ class Hibrido(Piloto):
     """Híbridos team. Inherits from Piloto"""
 
     def __init__(self, nombre, dinero=0, personalidad=choice(['precavido', 'osado']), 
-    contextura=randint(35, 54), equilibrio=randint(20, 34), n_exp=0, vehiculos = []):
+    contextura=randint(35, 54), equilibrio=randint(20, 34), experiencia=0, vehículos = []):
         """
         Arguments are ordered according to pilotos.csv original data. i.e.:
         Nombre, Dinero, Personalidad, Contextura, 
         Equilibrio, Experiencia, Equipo,
         Lista de Vehículos (obtained from vehículos.csv)
         """
-        super().__init__(nombre, dinero, n_exp, vehiculos)
+        super().__init__(nombre, dinero, experiencia, vehículos)
         self.contextura = contextura
         self.equilibrio = equilibrio
         self.personalidad = personalidad
@@ -172,14 +176,14 @@ class Docencio(Piloto):
     """Docencios team. Inherits from Piloto"""
         
     def __init__(self, nombre, dinero=0, personalidad='osado', 
-    contextura=randint(44, 60), equilibrio=randint(4, 10), n_exp=0, vehiculos = []):
+    contextura=randint(44, 60), equilibrio=randint(4, 10), experiencia=0, vehículos = []):
         """
         Arguments are ordered according to pilotos.csv original data. i.e.:
         Nombre, Dinero, Personalidad, Contextura, 
         Equilibrio, Experiencia, Equipo,
         Lista de Vehículos (obtained from vehículos.csv)
         """
-        super().__init__(nombre, dinero, n_exp, vehiculos)
+        super().__init__(nombre, dinero, experiencia, vehículos)
         self.contextura = contextura
         self.equilibrio = equilibrio
         self.personalidad = personalidad
