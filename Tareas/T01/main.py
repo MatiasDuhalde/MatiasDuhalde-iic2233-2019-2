e6580c65c397fb7514618c9ecd862ce2e433cb34
Matias Duhalde <matias.duhalde@uc.cl>
2019-09-12 04:21:04 -0300

    Add main loop and early gameflow

--- Tareas/T01/main.py
+++ Tareas/T01/main.py
@@ -1,10 +1,43 @@
 from lib.funciones import clear
 from lib.menu import MenuSesion, MenuPrincipal, MenuCarrera, \
     MenuCompraVehiculos, MenuPits, MenuPreparacionCarrera
+import time
 
 # -----------------------------------------------------------------------------
 #                                 GAME FLOW
 # -----------------------------------------------------------------------------
+
+menus = {}
+main_loop = True
+
 menu_sesion = MenuSesion()
-print(menu_sesion)
-a = menu_sesion.recibir_input()
+menu_actual = menu_sesion
+menus[type(menu_actual).__name__] = menu_actual
+
+while main_loop:
+    
+    sesion_loop = True
+    while sesion_loop:
+        print(menu_sesion)
+        
+        user_input = menu_sesion.recibir_input()
+        piloto = None
+        if user_input == 1:
+            piloto = menu_sesion.nueva_partida()
+        elif user_input == 2:
+            piloto = menu_sesion.cargar_partida()
+        if piloto:
+            sesion_loop = False
+            menu_actual = menu_sesion.go_to(user_input)(piloto)
+            print(type(menu_actual))
+            menus[type(menu_actual).__name__] = menu_actual
+
+    
+
+    
+    
+    print("HERE")
+    print(menus)
+    break
+
+
