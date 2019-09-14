a306be4b5fb93c056b310c71661fc7487cc2c0ff
Matias Duhalde <matias.duhalde@uc.cl>
2019-09-14 01:37:02 -0300

    Add default params to Piloto and Vehiculo

--- Tareas/T01/lib/carreras.py
+++ Tareas/T01/lib/carreras.py
@@ -27,42 +27,63 @@ class Vehiculo(ABC):
 
     """
     @abstractmethod
-    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, peso):
+    def __init__(self, nombre, dueño, chasis=None, carrocería=None, 
+    ruedas=None, peso=None, new_car=False):
         """Arguments are ordered according to pilotos.csv original data."""
         self.nombre = nombre
         self.dueño = dueño
-        self.chasis = chasis
-        self.carrocería = carrocería
-        self.ruedas = ruedas
-        self.peso = peso
+        if new_car:
+            self.chasis = None
+            self.carrocería = None
+            self.ruedas = None
+            self.peso = None
+        else:
+            self.chasis = int(chasis)
+            self.carrocería = int(carrocería)
+            self.ruedas = int(ruedas)
+            self.peso = int(peso)
+
 
 
 class Automovil(Vehiculo):
     
-    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, motor, peso):
-        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
-        self.motor = motor
+    def __init__(self, nombre, dueño, chasis=None, carrocería=None, 
+    ruedas=None, motor=None, peso=None, new_car=False):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso, new_car)
+        if new_car:
+            self.motor = None
+        else:
+            self.motor = int(motor)
 
 
 class Motocicleta(Vehiculo):
     
-    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, motor, peso):
-        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
-        self.motor = motor
+    def __init__(self, nombre, dueño, chasis=False, carrocería=False, 
+    ruedas=False, motor=False, peso=False, new_car=False):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso, new_car)
+        if new_car:
+            self.motor = None
+        else:
+            self.motor = int(motor)
 
 
 class Troncomovil(Vehiculo):
     
-    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, zapatillas, peso):
-        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
-        self.zapatillas = zapatillas
+    def __init__(self, nombre, dueño, chasis=False, carrocería=False, 
+    ruedas=False, zapatillas=False, peso=False, new_car=False):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso, new_car)
+        if new_car:
+            self.zapatillas = None
+        else:
+            self.zapatillas = int(zapatillas)
 
 
 class Bicicleta(Vehiculo):
     
-    def __init__(self, nombre, dueño, chasis, carrocería, ruedas, zapatillas, peso):
-        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso)
-        self.zapatillas = zapatillas
+    def __init__(self, nombre, dueño, chasis=False, carrocería=False, 
+    ruedas=False, zapatillas=False, peso=False, new_car=False):
+        super().__init__(nombre, dueño, chasis, carrocería, ruedas, peso, new_car)
+        self.zapatillas = int(zapatillas)
 
 
 
@@ -133,16 +154,16 @@ class Piloto:
 
         if new_pilot: 
             DEF_ARGS = pm.EQUIPOS[self.equipo.upper()]
-            self.dinero = 0
+            self.dinero = pm.DINERO_INICIAL
             self.personalidad = choice(DEF_ARGS['PERSONALIDAD'])
             self.contextura = randint(DEF_ARGS['CONTEXTURA']['MIN'], DEF_ARGS['CONTEXTURA']['MAX']) 
             self.equilibrio = randint(DEF_ARGS['EQUILIBRIO']['MIN'], DEF_ARGS['EQUILIBRIO']['MAX'])
             self.experiencia = 0
             self.vehículos = []
         else: 
-            self.dinero = dinero
+            self.dinero = int(dinero)
             self.personalidad = personalidad
-            self.contextura = contextura
-            self.equilibrio = equilibrio
-            self.experiencia = experiencia
+            self.contextura = int(contextura)
+            self.equilibrio = int(equilibrio)
+            self.experiencia = int(experiencia)
             self.vehículos = vehículos
