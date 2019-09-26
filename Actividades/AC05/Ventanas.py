ee951fe471fefa77efd842eab3495a96360bc115
Matias Duhalde <matias.duhalde@uc.cl>
2019-09-26 16:29:37 -0300

    Complete game, has bug with layout

--- Actividades/AC05/Ventanas.py
+++ Actividades/AC05/Ventanas.py
@@ -34,28 +34,22 @@ class VentanaJuego(QWidget):
         super().__init__()
         self.setGeometry(200,300, 300,300)
         self.show()
-
-    def actualizar_imagen(self):
-        pass
-
-    def keyPressEvent(self, event):
-        """
-        Este método maneja el evento que se produce al presionar las teclas.
-        Muestra la última letra presionada.
-        """
-        #self. asdasd .setText(f'{event.text()}')
-        self.label_letra.setText(event.text())
-        self.label_letra.resize(self.label_letra.sizeHint())
+        self.letra = ""
+        self.grilla = QGridLayout()
+        vbox = QVBoxLayout()
+        vbox.addLayout(self.grilla)
+        self.setLayout(vbox)
 
     def recibir_senal(self, datos):
         self.datos = datos
 
-        self.grilla = QGridLayout()
+        print(self.datos)
+        self.label_palabra = QLabel("PALABRA: " + self.datos["palabra"], self)
+        self.label_msg = QLabel(self.datos["msg"], self)
+        self.label_letra = QLabel(self.letra.upper(), self)
 
-        self.label_letra = QLabel(self.datos["msg"], self)
 
-
-        self.label_msg = QLabel('Ingresa una letra: ', self)
+        self.label_msg = QLabel(self.datos["msg"], self)
 
         self.label_letras_usadas = \
             QLabel(f'Letras usadas: \n{self.datos["usadas"]}', self)
@@ -64,14 +58,17 @@ class VentanaJuego(QWidget):
         
 
         self.boton_enviar = QPushButton('Ingresar Letra', self)
+        self.boton_enviar.clicked.connect(self.enviar_letra)
         self.boton_nuevo = QPushButton('Nuevo Juego', self)
+        self.boton_nuevo.clicked.connect(self.reiniciar_signal)
 
         self.imagen = QLabel(self)
         self.pixmap_imagen = QPixmap(self.datos["imagen"])
         self.imagen.setPixmap(self.pixmap_imagen)
 
         self.grilla.addWidget(self.imagen, *(0,0))
-        self.grilla.addWidget(self.label_msg, *(1,0))
+        self.grilla.addWidget(self.label_palabra, *(1,0))
+        self.grilla.addWidget(self.label_msg, *(2,0))
         self.grilla.addWidget(self.label_letra, *(4,0))
         self.grilla.addWidget(self.boton_enviar, *(5,0))
         self.grilla.addWidget(self.boton_nuevo, *(6,0))
@@ -79,6 +76,19 @@ class VentanaJuego(QWidget):
         self.grilla.addWidget(self.label_letras_disponibles, *(1,1))
 
 
-        vbox = QVBoxLayout()
-        vbox.addLayout(self.grilla)
-        self.setLayout(vbox)
+    def enviar_letra(self):
+        dict_letra = {
+            "letra": self.letra.upper(),
+            "palabra": None
+        }
+        self.enviar_letra_signal.emit(dict_letra)
+
+
+    def keyPressEvent(self, event):
+        """
+        Este método maneja el evento que se produce al presionar las teclas.
+        Muestra la última letra presionada.
+        """
+        self.letra = event.text().upper()
+        self.label_letra.setText(event.text())
+        self.label_letra.resize(self.label_letra.sizeHint())
