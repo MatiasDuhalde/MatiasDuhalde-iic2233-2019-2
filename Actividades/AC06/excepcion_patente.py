b6397df491d1fdff8641cb55234882d04e89cb60
Matias Duhalde <matias.duhalde@uc.cl>
2019-10-03 16:14:23 -0300

    AC06 - Complete part IV

--- Actividades/AC06/excepcion_patente.py
+++ Actividades/AC06/excepcion_patente.py
@@ -1,7 +1,10 @@
 class ErrorPatente(Exception):
-    def __init__(self, conductor):
+    def __init__(self, conductor, patente_oficial):
         '''
         Error si la patente no es la asociada al conductor.
         '''
-        pass
+        self.conductor = conductor
+        self.patente_oficial = patente_oficial
+        self.msg = (f"La patente {self.conductor.patente} no coincide con " + 
+        f"la del registro oficial ({self.patente_oficial}).")
 
