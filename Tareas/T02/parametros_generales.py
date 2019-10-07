8614a19570415790c71f86b7a858bfe67874d09c
Matias Duhalde <matias.duhalde@uc.cl>
2019-10-07 01:14:39 -0300

    T02 - Initial commit

---
+++ Tareas/T02/parametros_generales.py
@@ -0,0 +1,19 @@
+
+
+# Jugador
+MONEDAS_INICIALES = None        # Monedas con las que se parte
+VEL_MOVIMIENTO = None           # Pixeles avanzados por button press
+ENERGIA_JUGADOR = None          # Energía máxima
+ENERGIA_DORMIR = None           # Energía recuperada al dormir
+
+# Generación
+PROB_ARBOL = None               # Probabilidad de aparición de un árbol
+PROB_ORO = None                 # Probabilidad de aparición de oro
+
+# Tiempo
+# DEFINIR PARÁMETRO QUE CONTROLE QUE TAN RÁPIDO PASA EL TIEMPO (seccion 4.1)
+DURACION_LENA = None            # Duración de la leña antes de desaparecer
+DURACION_ORO = None             # Duración del oro antes de desaparecer
+
+# Mapa
+N = None                        # Dimensión del tile en mapa
