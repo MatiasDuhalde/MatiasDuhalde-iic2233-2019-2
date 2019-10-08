a5d338564069e02b45207abff3b266b993a7bafb
Matias Duhalde <matias.duhalde@uc.cl>
2019-10-07 22:16:24 -0300

    T02 - Add signal to go to main window from backend

--- Tareas/T02/backend.py
+++ Tareas/T02/backend.py
@@ -8,7 +8,12 @@ from PyQt5.QtCore import QObject, pyqtSignal
 
 class BackendInicio(QObject):
     """
-    asd
+    Procesador de ventana de inicio.
+
+    Señales:
+     - path_signal : recibe un string conteniendo un path desde el frontend
+     - feedback_signal : emite un string hacia el frontend, para actualizar el
+                         label de feedback de este (self.feedback_label)
     """
 
     path_signal = pyqtSignal(str)
@@ -16,6 +21,7 @@ class BackendInicio(QObject):
     def __init__(self):
         super().__init__()
         self.feedback_signal = None
+        self.goto_signal = None
         self.path_signal.connect(self.get_file)
 
     def get_file(self, path_mapa):
@@ -28,14 +34,18 @@ class BackendInicio(QObject):
         Es posible que un archivo no posea extensión en algunos sistemas
         operativos, por lo que este método no chequea si el input tenga una.
         """
+        path_mapa = path_mapa.strip()
         if "\\" in path_mapa:
             msg = "Path debe usar slash '/', no backslash '\\'"
+            self.feedback_signal.emit(msg)
+        elif path_mapa == "":
+            msg = ""
             self.feedback_signal.emit(msg)
         else:
             path_mapa = ["mapas"] + path_mapa.split("/")
             try:
                 with open(os.path.join(*path_mapa)) as mapa:
-                    pass
+                    self.goto_signal.emit()
                     # procesar...
                     # Agregar error de formato en mapa
 
--- Tareas/T02/frontend.py
+++ Tareas/T02/frontend.py
@@ -4,11 +4,11 @@ Este modulo contiene el frontend del programa
 
 import sys
 import os
-from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget,
                              QHBoxLayout, QVBoxLayout, QGridLayout,
                              QLabel, QPushButton, QLineEdit)
-from PyQt5.QtGui import (QPixmap, QColor)
+from PyQt5.QtGui import QPixmap
 from backend import BackendInicio
 
 
@@ -18,14 +18,22 @@ from backend import BackendInicio
 
 class WidgetCargar(QWidget):
     """
-    Widget central de la ventana de inicio
+    Widget central de la ventana de inicio.
+    Conecta directamente con el backend a través de señales.
+    También conecta con la ventana principal (MainGame), para iniciar el juego.
+
+    Señales:
+     - path_signal : emite un string conteniendo un path hacia el backend
+     - feedback_signal : recibe un string desde backend, para actualizar el
+                         label de feedback (self.feedback_label)
+     -
     """
-    update_signal = pyqtSignal(str)
+    feedback_signal = pyqtSignal(str)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.path_signal = None
-        self.update_signal.connect(self.update_feedback)
+        self.feedback_signal.connect(self.update_feedback)
         self.init_gui()
 
     def init_gui(self):
@@ -37,7 +45,6 @@ class WidgetCargar(QWidget):
             - Label de Error (feedback)
             - Push Button para iniciar
         """
-
         main_vbox = QVBoxLayout()
         main_vbox.addStretch(1)
 
@@ -80,36 +87,106 @@ class WidgetCargar(QWidget):
         self.setLayout(main_vbox)
 
     def enviar_path(self):
+        """
+        Envía el path ingresado por el usuario en self.input_path al backend.
+        """
         self.path_signal.emit(self.input_path.text())
 
     def update_feedback(self, texto):
+        """
+        Actualiza el mensaje de feedback, según lo enviado luego de
+        self.enviar_path por el backend.
+        """
         self.feedback_label.setText(texto)
+
+
+class MainGame(QWidget):
+    """
+    Widget central de la ventana principal. Aquí se desarrolla el juego y se
+    muestran los elementos gráficos.
+    Conecta directamente con el backend a través de señales.
+    También conecta con la ventana principal (MainGame), la ventana de tienda y
+    la ventana de la casa.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.init_gui()
+
+    def init_gui(self):
+        """
+        Inicializa los elementos gráficos y funcionales del widget:
+            - ...
+            - ...
+        """
+        main_vbox = QVBoxLayout()
+
+        self.label = QLabel("Testing")
+        main_vbox.addWidget(self.label)
+        self.setLayout(main_vbox)
 
 
 class VenanaInicio(QMainWindow):
     """
-    Main Window de la ventana de inicio
+    Main Window de la ventana de inicio.
+    Funciona como base para tener consistencia con la estructura, no tiene
+    función real aparte de contener al widget central (WidgetCargar)
     """
+    goto_principal_signal = pyqtSignal()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setWindowTitle("Ventana de Juego")
+        self.setWindowTitle("Ventana de Carga")
+
+        # Instancia ventana principal (para activar desde self.goto_principal)
+        self.ventana_principal = None
+        # Instanciar elementos
         self.ventana_cargar = WidgetCargar()
+        self.backend = BackendInicio()
+        # Conectar Señales
+        self.goto_principal_signal.connect(self.goto_principal)
+        self.backend.feedback_signal = self.ventana_cargar.feedback_signal
+        self.ventana_cargar.path_signal = self.backend.path_signal
+        self.backend.goto_signal = self.goto_principal_signal
+
+        # Determina Tamaño y posición
+        self.setGeometry(780, 415, 420, 250)
+
+        # Agregar widget a Main Window
         self.setCentralWidget(self.ventana_cargar)
 
-        self.show()
+    def goto_principal(self):
+        """
+        Método ejecutado al cargar mapa correctamente desde backend.
+        Esconde la ventana de inicio y abre la principal.
+        """
+        self.hide()
+        self.ventana_principal.show()
+
+
+class VentanaPrincipal(QMainWindow):
+    """
+    Main Window de la ventana principal.
+    Funciona como base para tener consistencia con la estructura, no tiene
+    función real aparte de contener al widget central (WidgetCargar)
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.juego = MainGame()
+        self.setCentralWidget(self.juego)
 
 
 if __name__ == '__main__':
     app = QApplication([])
 
-    # Ventana de inicio
-    backend_inicio = BackendInicio()
+    # Creación de ventanas
     ventana_inicio = VenanaInicio()
-    backend_inicio.feedback_signal = ventana_inicio.ventana_cargar.update_signal
-    ventana_inicio.ventana_cargar.path_signal = backend_inicio.path_signal
-
-    ventana_principal = None
+    ventana_principal = VentanaPrincipal()
     ventana_tienda = None
+
+    # Conectar ventana inicio con ventana principal
+    ventana_inicio.ventana_principal = ventana_principal
+
+    # start
+    ventana_inicio.show()
 
     sys.exit(app.exec_())
