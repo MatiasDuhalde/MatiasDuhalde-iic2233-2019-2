3ce32e48542d02fd7bbd0394106ae6e3b588951a
Matias Duhalde <matias.duhalde@uc.cl>
2019-11-26 16:15:25 -0300

    Add client main file

--- Tareas/T03/client/main.py
+++ Tareas/T03/client/main.py
@@ -1,8 +1,17 @@
+"""
+Main script del cliente.
+"""
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui_inicio import VentanaInicio
+from client import Client
 
 if __name__ == '__main__':
     app = QApplication([])
-    form = VentanaInicio()
+    ventana_inicio = VentanaInicio()
+    client = Client()
+
+    # Connect signal
+    # ventana_inicio.username_signal = 
+
     sys.exit(app.exec_())
