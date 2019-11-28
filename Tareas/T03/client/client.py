f3d5fc7a6060c7767304ec26cb2dddd259694a8c
Matias Duhalde <matias.duhalde@uc.cl>
2019-11-28 17:31:51 -0300

    T03 - Complete client-side message exchange

--- Tareas/T03/client/client.py
+++ Tareas/T03/client/client.py
@@ -1,83 +1,65 @@
-__author__ = "chapito96 & fringlesinthestreet" + " & StroveLight"
-
+"""
+Código y funcionamiento del cliente.
+"""
 import threading
 import socket
+import os
 import json
-import sys
+import time
 from PyQt5.QtCore import pyqtSignal, QObject
-from PyQt5.QtWidgets import QApplication
+from parametros import PARAMETROS
 
-class Client(QObject):
-
-    '''
-    Esta es la clase encargada de conectarse con el servidor e intercambiar información
-    Hereda de QObject con el único motivo de poder emitir y conectar señales.
-    Se recomienda que exista un BackEnd aparte del cliente (y que se relacionen).
-    '''
-
-    # Señal para avisar cuando llegan resultados del servidor
-    enviar_a_interfaz = pyqtSignal(dict)
+class Client():
+    """
+    Esta clase funciona como conexión entre el backend del programa y el
+    servidor.
+    """
 
     def __init__(self):
+        self.start_time = time.strftime(r"%y-%m-%d %H.%M.%S")
+        self.log("Inicializando cliente...")
 
-        # Como heredamos de QObject hay que hacer el llamado a super()
-        super().__init__()
-        print("Inicializando cliente...")
-
-        # Inicializamos el socket principal del cliente.
-        # Este corresponde al de una conexión TCP
+        # Communication attributes
+        self.host = PARAMETROS["host"]
+        self.port = PARAMETROS["port"]
         self.socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Primero definimos la dirección a la cual nos conectaremos.
-        # En este caso se trabaja de manera local
-        self.host = "localhost"
-
-        # Definimos un Puerto al cual será conectado el cliente
-        self.port = 1238
+        # Other attributes
+        self.username = None
+        self.connected = False
 
         try:
-            # Primero nos conectamos al servidor, pasándole como argumento la tupla
-            # (HOST, PORT) al cual nos queremos conectar.
-            # Esto tira error si la conexión es privada o si no hay un servidor "escuchando"
             self.socket_cliente.connect((self.host, self.port))
-            print("Cliente conectado exitosamente al servidor...")
-
-            # hacemos True un boolean para que escuche
+            self.log(f"Cliente conectado exitosamente al servidor en {self.host}:{self.port}")
             self.connected = True
 
-            # Luego, creamos un thread para comenzar a escuchar lo que nos envía el servidor
-            # Usamos un thread para permitir que el programa realice otras cosas
-            # mientras escucha al servidor
+            # Thread para interactuar con el servidor
             thread = threading.Thread(target=self.listen_thread, daemon=True)
             thread.start()
-            print("Escuchando al servidor...")
-
-            # Finalmente, conectamos la señal con un método de la ventana
-            self.interfaz = Interfaz()
-            self.interfaz.senal_a_backend.connect(self.enviar_al_servidor)
-            self.enviar_a_interfaz.connect(self.interfaz.desplegar_resultado)
-
+            self.log("Escuchando al servidor...")
         except ConnectionRefusedError:
-            # Si la conexión es rechazada, entonces se 'cierra' el socket
-            print("No se encontró un servidor\nAbortando...")
+            self.log(f"No se encontró un servidor en {self.host}:{self.port}")
+            self.log("Cerrando cliente")
             self.socket_cliente.close()
             exit()
 
     def listen_thread(self):
-        '''
+        """
         Este método es el usado en el thread y la idea es que reciba lo que
         envía el servidor. Implementa el protocolo de agregar los primeros
         4 bytes, que indican el largo del mensaje
         :return:
-        '''
+
+        TODO
+        ESTA WEA NO SIRVE JJJJJJJAAAAAAAAAA
+        """
 
         # Si desean que un usuario pueda desconectarse
         while self.connected:
             # Primero recibimos los 4 bytes del largo
             response_bytes_length = self.socket_cliente.recv(4)
             # Los decodificamos
-            response_length = int.from_bytes(response_bytes_length,
-                                             byteorder="big")
+            response_length = int.from_bytes(response_bytes_length, byteorder="little")
 
             # Luego, creamos un bytearray vacío para juntar el mensaje
             response_bytes = bytearray()
@@ -98,52 +80,120 @@ class Client(QObject):
             # realizará en otro método
             self.manejar_comando(decoded)
 
-    def manejar_comando(self, mensaje):
-        '''
-        :param mensaje: diccionario con la información
-        :return:
-        '''
-        # Podemos imprimir para verificar que toodo anda bien
-        print("Mensaje Recibido: {}".format(mensaje))
+    @staticmethod
+    def encode_message(msg):
+        """
+        Transforma un mensaje en chunks. Este se serializa usando JSON, y luego
+        se codifica según el protocolo descrito en el enunciado:
+         - Primeros 4 bytes indican el largo del contenido del mensaje, en
+           little endian.
+         - Los chunks que representan el mensaje se separan en 124 bytes.
+         - Cada chunk es antecedido por el número de chunk, 4 bytes en
+           big endian.
+         - Si un chunk tiene menos de 124 bytes, estos se rellenan con ceros
+           a la derecha.
 
-        self.enviar_a_interfaz.emit(mensaje)
+        Parámetros:
+         - msg: Objeto a codificar
 
+        Retorna una lista de cadenas de bytes según el protocolo.
 
-    def send(self, msg):
-        '''
-        :param msg: diccionario con la información
-        :return:
-        '''
-
-        # Le hacemos json.dumps y luego lo transformamos a bytes
+        VER: https://github.com/IIC2233/syllabus/issues/546#issuecomment-559302753
+        """
         msg_json = json.dumps(msg)
         msg_bytes = msg_json.encode()
 
-        # Luego tomamos el largo de los bytes y creamos 4 bytes de esto
-        msg_length = len(msg_bytes).to_bytes(4, byteorder="big")
+        msg_length = len(msg_bytes)
+        msg_length_bytes = msg_length.to_bytes(4, byteorder="little")
 
-        # Finalmente, los enviamos al servidor
-        self.socket_cliente.send(msg_length + msg_bytes)
+        blocks = [msg_length_bytes]
+        for i in range(0, msg_length, 124):
+            n_chunk = i//124 + 1
+            n_chunk_bytes = n_chunk.to_bytes(4, byteorder="big")
+            chunk_bytes = msg_bytes[i:80+i]
+            if len(chunk_bytes) < 124:
+                chunk_bytes += b"\x00"*(124 - len(chunk_bytes))
+            blocks.append(n_chunk_bytes)
+            blocks.append(chunk_bytes)
+        return blocks
 
+    def send(self, username=None, action="nothing"):
+        """
+        Este método se encarga de comunicarse con el servidor. La información
+        es indexada dentro de un diccionario, el cual se codifica y se manda.
 
-    def enviar_al_servidor(self, palabra):
-        '''
-        Este método es el que se gatilla con la señal y manda la información al método send
-        :param palabra: string que representa la palabra a japonizar
-        :return:
-        '''
+        Parámetros:
+         - username: Nombre de usuario
 
-        # Tomamos la información del evento y la pasamos al formato antes descrito
-        data = {"palabra": palabra}
+        TODO
+        AGREGAR LOS OTROS PARÁMETROS
+        """
 
-        # Llamamos al método send para enviar la info al servidor
-        self.send(data)
+        dict_ = {
+            "username": username,
+            "action": action
+        }
 
+        msg_encoded = Client.encode_message(dict_)
+        for chunk in msg_encoded:
+            self.socket_cliente.send(chunk)
 
+    @staticmethod
+    def decode_message(msg):
+        """
+        Transforma una cadena de bytes en un objeto (revisar protocolo en
+        self.encode_message).
 
-if __name__ == "__main__":
-    app = QApplication([])
+        Parámetros:
+         - msg: Bytes a decodificar
 
-    client = Client()
+        Retorna el objeto decodificado.
+        """
+        msg_json = msg.decode(encoding="utf-8")
+        decoded_msg = json.loads(msg_json)
 
-    sys.exit(app.exec_())
+        return decoded_msg
+
+    def receive(self):
+        """
+        Este método se encarga de comunicarse con el servidor. La información
+        es indexada dentro de un diccionario, el cual se codifica y se manda.
+
+        Parámetros:
+         - username: Nombre de usuario
+
+        Retorna el objeto recibido y decodificado.
+        """
+        # Recibir largo del mensaje (bytes)
+        msg_length_bytes = self.socket_cliente.recv(4)
+        msg_length = int.from_bytes(msg_length_bytes, byteorder="little")
+
+        msg = bytearray()
+        proof_counter = 1
+        while len(msg) < msg_length:
+            bytes_msg = self.socket_cliente.recv(128)
+            index = int.from_bytes(bytes_msg[0:4], byteorder="big")
+            if proof_counter != index:
+                error_msg = "Mensaje no recibido correctamente"
+                self.log(error_msg)
+                raise ValueError(error_msg)
+            proof_counter += 1
+            msg += bytes_msg[4:]
+
+        return self.decode_message(msg)
+
+    def log(self, msg, end="\n"):
+        """
+        Escribe en el archivo txt de log/registro.
+
+        Parámetros:
+         - msg: Mensaje a escribir en el log
+        """
+        filename_ = f'[{self.start_time}] client log.txt'
+        if os.path.isfile(filename_):
+            mode = "a"
+        else:
+            mode = "w"
+        with open(filename_, mode) as log_file:
+            time_ = time.strftime(r"%y-%m-%d %H:%M:%S")
+            log_file.write(f"[{time_}] {msg}{end}")
