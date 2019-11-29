72b3247b2c72daca323b1a9f5fd724c5ff431d29
Matias Duhalde <matias.duhalde@uc.cl>
2019-11-28 21:38:25 -0300

    T03 - Remove print tests and signal statements

--- Tareas/T03/client/main.py
+++ Tareas/T03/client/main.py
@@ -4,16 +4,9 @@ Main script del cliente.
 import sys
 from PyQt5.QtWidgets import QApplication
 from client import Client
-from backend import Backend
-from gui_inicio import VentanaInicio
 
 if __name__ == '__main__':
     APP = QApplication([])
-
     CLIENT = Client()
-    print("jaja")
-    # Connect signal
-    VENTANA_INICIO.username_signal = BACKEND.username_signal
-    BACKEND.feedback_signal = VENTANA_INICIO.feedback_signal
 
     sys.exit(APP.exec_())
