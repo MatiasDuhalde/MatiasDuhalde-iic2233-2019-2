7ab59afcedd8cba705f5628a14414060d468ab63
Matias Duhalde <matias.duhalde@uc.cl>
2019-11-28 21:37:14 -0300

    T03 - Add user retrieving functions

--- Tareas/T03/server/usuarios.py
+++ Tareas/T03/server/usuarios.py
@@ -11,43 +11,47 @@ class Usuario:
     Atributos:
      - Nombre
      - Personaje
+
+    Métodos:
+     - get_usuarios
+     - get_amigos
     """
     def __init__(self, nombre, personaje):
         self.nombre = nombre
         self.personaje = personaje
-        self.amigos = get_amigos(self.nombre)
+        self.amigos = self.get_amigos(self)
+
+    @staticmethod
+    def get_usuarios():
+        """
+        Retorna una lista de instancias de la clase Usuario según los
+        contenidos de usuarios.json.
+        """
+        with open(PARAMETROS["path usuarios"], 'r', encoding='utf-8-sig') as file_usuarios:
+            usuarios = []
+            for info in json.load(file_usuarios):
+                usuarios.append(Usuario(**info))
+        return usuarios
+
+    @staticmethod
+    def get_amigos(usuario):
+        """
+        Retorna una lista que contiene los nombres de usuario de los amigos del
+        nombre de usuario pedido, según los contenidos de amigos.json
+        """
+        if isinstance(usuario, Usuario):
+            usuario = usuario.nombre
+        elif isinstance(usuario, str):
+            pass
+        else:
+            raise TypeError
+        with open(PARAMETROS["path amigos"], 'r', encoding='utf-8-sig') as file_usuarios:
+            dict_amigos = json.load(file_usuarios)
+            if usuario in dict_amigos:
+                amigos = dict_amigos[usuario]
+            else:
+                amigos = []
+        return amigos
 
     def __repr__(self):
         return self.nombre
-
-
-def get_usuarios():
-    """
-    Retorna una lista de instancias de la clase Usuario según los contenidos
-    de usuarios.json.
-    """
-    with open(PARAMETROS["path usuarios"], 'r', encoding='utf-8-sig') as file_usuarios:
-        usuarios = []
-        for info in json.load(file_usuarios):
-            usuarios.append(Usuario(**info))
-
-    return usuarios
-
-def get_amigos(usuario):
-    """
-    Retorna una lista que contiene los nombres de usuario de los amigos del
-    nombre de usuario pedido, según los contenidos de amigos.json
-    """
-    if isinstance(usuario, Usuario):
-        usuario = usuario.nombre
-    elif isinstance(usuario, str):
-        pass
-    else:
-        raise TypeError
-    with open(PARAMETROS["path amigos"], 'r', encoding='utf-8-sig') as file_usuarios:
-        dict_amigos = json.load(file_usuarios)
-        if usuario in dict_amigos:
-            amigos = dict_amigos[usuario]
-        else:
-            amigos = []
-    return amigos
